7f50df221e144de7ee805f944f1cee436f5505af
João Victor Pessoa <jvpessoa10@hotmail.com>
2019-05-03 14:47:41 -0300

    reduzindo qualidade e separando fotos

--- PhotoSeparator.py
+++ PhotoSeparator.py
@@ -10,49 +10,58 @@ class PhotoSeparator:
     def __init__(self, pf, dp):
         self.PHOTOS_PATH = pf + '/'
         self.DESTINATION_PATH = dp + '/'
+        self.REDUCED_PATH = self.DESTINATION_PATH+'reduced/'
+
+        self.LOW_PATH = self.DESTINATION_PATH+'low/'        
+        self.NOMAL_PATH = self.DESTINATION_PATH+'normal/'
         self.photos = os.listdir(self.PHOTOS_PATH)
         self.raw_encodes = []
         self.known_people = []
         self.people_final = []
 
+
+    def reduceQuality(self):
+        os.mkdir(self.DESTINATION_PATH)
+        os.mkdir(self.REDUCED_PATH)
+
+        for index,image in enumerate(self.photos):
+            path = self.PHOTOS_PATH+image
+            im = Image.open(path) 
+            wpercent = (600 / float(im.size[0]))
+            hsize = int((float(im.size[1]) * float(wpercent)))
+
+            im = im.resize((600, hsize), PIL.Image.ANTIALIAS)
+            im.save(self.REDUCED_PATH +image, quality = 70)
+
+         
+
+
     def separateEncodes(self):
+        
         for index, image in enumerate(self.photos):
+                print(index+1," de ",len(self.photos)," fotos")
             
-                image_not_pass = False
-                while True:
+                path =  self.REDUCED_PATH+ image
+                image_file = face_recognition.load_image_file(path)
+            
+                try:
+                    encodings = face_recognition.face_encodings(image_file)
+
+                    self.raw_encodes.append(encodings)
                     
-                    path = self.PHOTOS_PATH+image
-
-
-                    if(image_not_pass):
-                        im = Image.open(path) 
-                        wpercent = (300 / float(im.size[0]))
-                        hsize = int((float(im.size[1]) * float(wpercent)))
-                        im = im.resize((300, hsize), PIL.Image.ANTIALIAS)
-                        im.save(path, quality = 10)
-
+                except IndexError:
+                    print("Foto sem rostos:", image)
+                except MemoryError:
+                    print("Foto muito pesada, reduzindo...")
+                    image_not_pass = True
+                else:
+                    print("Pronto")
                     
-                    
-
-                    image_file = face_recognition.load_image_file(path)
-                    print("Analisando imagem",image," ...")
-                    try:
-                        encodings = face_recognition.face_encodings(image_file)
-
-                        self.raw_encodes.append(encodings)
-                        
-                    except IndexError:
-                        print("Foto sem rostos:", image)
-                    except MemoryError:
-                        print("Foto muito pesada, reduzindo...")
-                        image_not_pass = True
-                    else:
-                        print("Pronto")
-                        break
                     
                 
     
     def analyseEncodes(self):
+        print("Comparando rostos...")
         i = 0
         for photo_encodes in self.raw_encodes:
 
@@ -78,14 +87,20 @@ class PhotoSeparator:
                     self.known_people.append(encode)
                     self.people_final.append(encode_photos)
             i += 1
+
     def separatePhotos(self):
-        os.mkdir(self.DESTINATION_PATH)
+        
+        os.mkdir(self.NOMAL_PATH)
+        os.mkdir(self.LOW_PATH)
         for i,people in enumerate(self.people_final):
             
-            os.mkdir(self.DESTINATION_PATH+str(i))
+            os.mkdir(self.NOMAL_PATH+str(i))
+            os.mkdir(self.LOW_PATH+str(i))
+
             for photo_i in people: 
-                shutil.copy(self.PHOTOS_PATH+self.photos[photo_i], self.DESTINATION_PATH+str(i)+'/')
-        print("Pronto")
+                shutil.copy(self.PHOTOS_PATH+self.photos[photo_i], self.NOMAL_PATH+str(i)+'/')
+                shutil.copy(self.REDUCED_PATH+self.photos[photo_i], self.LOW_PATH+str(i)+'/')
+        
 
     def clear(self):
         self.raw_encodes = []
@@ -93,13 +108,17 @@ class PhotoSeparator:
         self.people_final = []
     
     def start(self):
+        self.reduceQuality()
         self.separateEncodes()
         self.analyseEncodes()
         self.separatePhotos()
-        self.clear()
+        self.clear() 
                 
     
     
+
+
+
 
 
 
